5d89a443cfcd5d94d514775769704221c281715e
Yatharth Kale <54075161+yatharth23112001@users.noreply.github.com>
2022-06-26 19:32:31 +0530

    finalized sign signup form ui

--- client/src/components/Sign_up.jsx
+++ client/src/components/Sign_up.jsx
@@ -3,16 +3,40 @@ import React from 'react'
 const Sign_up = () => {
   return (
     <>
-        <div class="relative mb-4">
-        <label for="full-name" class="leading-7 text-sm text-gray-400">Full Name</label>
-        <input type="text" id="full-name" name="full-name" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      <div class="md:flex md:items-center mb-10">
+        <div class="md:w-1/3">
+          <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+            Full Name
+          </label>
         </div>
-        <div class="relative mb-4">
-        <label for="email" class="leading-7 text-sm text-gray-400">Email</label>
-        <input type="email" id="email" name="email" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <div class="md:w-2/3">
+          <input type="text" id="full-name" name="full-name" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
         </div>
-        <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-        <p class="text-xs mt-3">Literally you probably haven't heard of them jean shorts.</p>
+      </div>
+
+      <div class="md:flex md:items-center mb-10">
+        <div class="md:w-1/3">
+          <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+            Email Id
+          </label>
+        </div>
+        <div class="md:w-2/3">
+          <input type="email" id="email" name="email" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+        </div>
+      </div>
+
+      <div class="md:flex md:items-center mb-10">
+        <div class="md:w-1/3">
+          <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+            Password
+          </label>
+        </div>
+        <div class="md:w-2/3">
+          <input type="email" id="email" name="email" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+        </div>
+      </div>
+
+      <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Register</button>
     </>
   )
 }
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,10 +4,31 @@ import { useState } from 'react'
 
 const Welcome = () => {
 
-    const[components, setComponents] = useState({
-        "sign_in": true,
-        "sign_up": false
-    }) 
+  const[components, setComponents] = useState({
+      "sign_in": true,
+      "sign_up": false
+  }) 
+
+  const [signInStyle, setsignInStyle] = useState("text-white font-bold py-2 px-4 border-b-4 border-indigo-500 hover:border-blue-500 rounded");
+  const [signUpStyle, setsignUpStyle] = useState("hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded opacity-50");
+
+  const changeSignInStyle = ()=>{
+    setsignInStyle("text-white font-bold py-2 px-4 border-b-4 border-indigo-500 hover:border-blue-500 rounded");
+    setsignUpStyle("hover:bg-indigo-500  text-white font-bold py-2 px-4 rounded opacity-50");
+    setComponents({
+      "sign_in": true,
+      "sign_up": false
+    })
+  }
+
+  const changeSignUpStyle = ()=>{
+    setsignInStyle("hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded opacity-50");
+    setsignUpStyle("text-white font-bold py-2 px-4 border-b-4 border-indigo-500 hover:border-blue-500 rounded");
+    setComponents({
+      "sign_in": false,
+      "sign_up": true
+    })
+  }
 
   return (
     <section class="text-gray-400 bg-gray-900 body-font">
@@ -18,11 +39,11 @@ const Welcome = () => {
         </div>
 
         <div class="text-white lg:w-2/6 md:w-1/2 bg-gray-800 bg-opacity-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-            <div class="grid grid-cols-2 mb-5">
-                <button class="hover:bg-indigo-500 text-white font-bold py-2 px-4 border-b-4 border-indigo-500 hover:border-blue-500 rounded">
+            <div class="grid grid-cols-2 mb-10">
+                <button className={signInStyle} onClick={changeSignInStyle}>
                     SignIn
                 </button>
-                <button class="text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed">
+                <button className={signUpStyle} onClick={changeSignUpStyle}>
                     SignUp
                 </button>
             </div>
